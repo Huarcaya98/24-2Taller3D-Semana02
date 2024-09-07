aa72ca97abe3c7cef3897c645d1338613ac4d7b5
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-09-06 19:28:41 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._2Taller3D_Semana02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
