4f98465e9d4683a9b5bc7303a78595bedd82def1
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-09-06 19:39:46 -0500

    Created Class Figura

---
+++ Figura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._2Taller3D_Semana02
+{
+    internal class Figura
+    {
+
+        public virtual  float calcArea()
+        {
+            return 0;
+        }
+
+        public virtual float calcPerimetro()
+        {
+            return 0;
+        }
+
+    }
+}
